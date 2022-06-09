197972399ba1a11cae8ff736cdea8f3619f3c94b
= <=>
2022-06-08 22:46:27 -0300

    first commit

---
+++ src/Form.test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Form from "./Form.js"
+
+describe('Form', () => {
+  it('renders without crashing', () => {
+    render(<Form />)
+  })
+  expect(Form).toBeDefined()
+})
