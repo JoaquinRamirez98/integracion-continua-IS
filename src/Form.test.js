a14a5b1bd01f1a0b6579a92f74c82963562b6361
Ramirez Joaquin <101899509+JoaquinRamirez98@users.noreply.github.com>
2022-06-13 18:55:04 -0300

    solucion

--- src/Form.test.js
+++ src/Form.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
-import Form from "./Ffform"
+import Form from "./Form"
 
 describe('Form', () => {
   it('renders without crashing', () => {
