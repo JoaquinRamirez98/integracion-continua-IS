3c9d36b2e2d1ec4e4f35b2c777803091f81bc750
= <=>
2022-06-13 21:57:53 -0300

    esto es un error 1

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-class Form extends Component {
+class Fo extends Component {
     constructor(props) {
         super(props);
         
--- src/Form.test.js
+++ src/Form.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
-import Form from "
+import Form from "./Form"
 
 describe('Form', () => {
   it('renders without crashing', () => {
